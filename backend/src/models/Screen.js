54cdab8050ca285ebfddf17c1939516d25933375
FranceMokoena <developer091120@gmail.com>
2025-08-14 17:18:32 -0700

    10001

--- backend/src/models/Screen.js
+++ backend/src/models/Screen.js
@@ -1,51 +1,16 @@
-// Screen.js
-// Screen.js
-import mongoose from 'mongoose';
+// src/models/Screen.js
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importStar(require("mongoose"));
-const screenSchema = new mongoose_1.Schema({
-    barcode: { type: String, required: true },
-    status: { 
-        type: String, 
-        enum: ['Reparable', 'Beyond Repair', 'Healthy'], 
-        required: true
-    },
-    timestamp: { type: Date, default: () => new Date() },
-    session: { type: mongoose_1.Schema.Types.ObjectId, ref: 'TaskSession', required: true },
+const screenSchema = new Schema({
+  barcode: { type: String, required: true },
+  status: { 
+    type: String, 
+    enum: ['Reparable', 'Beyond Repair', 'Healthy'], 
+    required: true
+  },
+  timestamp: { type: Date, default: () => new Date() },
+  session: { type: Schema.Types.ObjectId, ref: 'TaskSession', required: true },
 }, { timestamps: true });
-export default mongoose.model('Screen', screenSchema);
-//# sourceMappingURL=Screen.js.map
+
+module.exports = mongoose.model('Screen', screenSchema);
